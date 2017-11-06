bf89892be905e0890b8e40ce3bf90199aaa61d07
Huy Hoang Bui <buihh@myumanitoba.ca>
2017-11-06 00:42:40 -0600

    Fixed previous version

--- bluetoothComm/socketClient.py
+++ bluetoothComm/socketClient.py
@@ -1,19 +1,17 @@
 """
-	A simple Python script to send messages to a server over 
-	Bluetooth using Python sockets (with Python 3.3 or above)
+A simple Python script to send messages to a sever over Bluetooth
+using PyBluez (with Python 2).
 """
 
-import socket
+import bluetooth
 
 serverMACAddress = '5C:F3:70:76:B6:5E'
-port = 
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
-	socket.BTPROTO_RFCOMM)
-s.connect(serverMACAddress, port)
+port = 3
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((serverMACAddress, port))
 while 1:
-	text = input()
-	if text == "quit":
-		break
-	s.send(bytes(text, 'UTF-8'))
-s.close()
-
+    text = raw_input() # Note change to the old (Python 2) raw_input
+    if text == "quit":
+    break
+    s.send(text)
+sock.close()
