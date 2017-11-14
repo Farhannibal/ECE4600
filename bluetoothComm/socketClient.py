3d745342087b2c9d99d1b02485b5b49b74090c8e
Huy Hoang Bui <buihh@myumanitoba.ca>
2017-11-13 21:08:00 -0600

    Print out the command to the text file for execution

--- bluetoothComm/socketClient.py
+++ bluetoothComm/socketClient.py
@@ -1,17 +1,43 @@
 """
 A simple Python script to send messages to a sever over Bluetooth
-using PyBluez (with Python 2).
+using PyBluez
 """
 
 import bluetooth
+from commandList import listOfCommand
 
 serverMACAddress = '5C:F3:70:76:B6:5E'
 port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((serverMACAddress, port))
-while 1:
-    text = input()
-    if text == "quit":
-   		break
-    s.send(text)
-sock.close()
+output_file = open("CommandQueue.txt", "w")
+
+# Establish connection between server and client
+if (s.connect((serverMACAddress, port)))
+	println("Connection established")
+	while 1:
+    	data = client.recv(size)
+        output_file.write(data)
+
+		if data == "quit":
+			print("Closing server ...")
+			break
+		elif listOfCommand(data) == 1:
+			print("Go up")
+		elif listOfCommand(data) == 2:
+			print("Go back")
+		elif listOfCommand(data) == 3:
+			print("Turn left")
+		elif listOfCommand(data) == 4:
+			print("Turn right")
+		elif listOfCommand(data) == 5:
+			print("Stop the car!")
+		else:
+			print("Command is not available")
+	        client.send(data) # Echo back to client
+
+	s.close() # When the server close the socket     
+else 
+	println("Error connecting to host")
+	break
+	
+output_file.close()
--- bluetoothComm/socketServer.py
+++ bluetoothComm/socketServer.py
@@ -4,24 +4,34 @@ Bluetooth using PyBluez.
 """
 
 import bluetooth
+from commandList import listOfCommand
 
+# Definition of some basic data and address
 hostMACAddress = '5C:F3:70:76:B6:5E' # The MAC address of a Bluetooth adapter 
 					#on the server. The server might have 
 					#multiple Bluetooth adapters.
 port = 3
 backlog = 1
 size = 1024
+
+# Establish connection between server and client
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((hostMACAddress, port))
+
 s.listen(backlog)
+
 try:
     client, clientInfo = s.accept()
     while 1:
-        data = client.recv(size)
-        if data:
-            print(data)
-            client.send(data) # Echo back to client
+    	text = input()
+    	# Quit when user type quit in command line
+    	s.send(text) # close both client and server 
+    	if text == "quit":
+   			break
+	
+        
 except:	
     print("Closing socket")
     client.close()
-    s.close()
+   
+s.close()
