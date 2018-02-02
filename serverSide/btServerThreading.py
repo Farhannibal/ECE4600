a2588e024528e1fac038f12e876d5c6024ae1b1e
Huy Hoang Bui <buihh@myumanitoba.ca>
2018-02-01 22:12:44 -0600

    Fix minor merge change

--- serverSide/btServerThreading.py
+++ serverSide/btServerThreading.py
@@ -1,4 +1,5 @@
 import bluetooth
+import time
 from threading import Thread
 
 # Library of command
@@ -21,6 +22,7 @@ class ThreadedServer(Thread):
         self.port = port
         self.btConnect = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         self.btConnect.bind((hostMACAddress, port))
+        self.numberOfConnection = 0
         print("Server is up!")
 
     def listen(self):
@@ -32,35 +34,61 @@ class ThreadedServer(Thread):
             client.settimeout(60)
             Thread(target = self.listenToClient, args = (client, address)).start()
 
-    def listenToClient(self, client, address):
-       
+    # Method to establish the new connection for each thread
+    def listenToClient(self, client, clientInfo):
         dataSize = 1024
-        # First check if client finish estasblishing connection
+         # First check if client finish estasblishing connection
         client.send("ACK4S")
         serverDataRecv = client.recv(dataSize)
         serverDataRecv = serverDataRecv.decode("utf-8")
 
+        #control = 0
+        control = 2
+
+        counter = 1
+        queue = []
+
         # Second ACK the connection from client
         if listOfCommand(serverDataRecv) == 1:
-            print("Connection established for "+str(address))
+            print("Connection established for "+str(clientInfo))
+            self.numberOfConnection += 1
             while True:
                 # After establish connection, now start command client
-                data = input("Enter command for client: "+str(address))
+                if control == 0:
+                    queue = str(json.load(open("Traffic_Sim/Assets/CarData.json"))["commands"]).split(',')
+                    control = 1
+                    data = queue[0]
+                elif len(queue) > counter:
+                    data = queue[counter]
+                    counter = counter + 1
+                else:
+                    print("Please enter command for "+str(clientInfo)+": ")
+                    print("Available commands are UP, DOWN, LEFT, RIGHT, STOP or QUIT: ")
+                    data = input()
+
                 if listOfCommand(data) != 99:
-                    # If data is valid then start sending
+                # If data is valid then start sending
+
+                #Also timing the connection time
+                    start = time.time()
                     client.send(data)
                     
                     # Waiting for update on position
                     serverDataRecv = client.recv(dataSize).decode("utf-8")
+                    end = time.time()
+
+                    print("Time needed for update = " + str(end-start))
+
                     print(serverDataRecv)
-                    with open('Traffic_Sim/Assets/data.json', 'w') as outfile:
-                    #with open('data.json', 'w') as outfile:
+                    #with open('Traffic_Sim/Assets/data.json', 'w') as outfile:
+                    with open('data.json', 'w') as outfile:
                         outfile.write(serverDataRecv)
                     # Quit when user type quit in command line
                     if data == "QUIT":
                         break
                 else:
                     print("Wrong command, please check the command list and repeat input !")
+            client.close()
         return False
 
 if __name__ == "__main__":
