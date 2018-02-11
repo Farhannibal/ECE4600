4c61724092d4b3fae8ae4cf2629b8283de9ce71d
Huy Hoang Bui <buihh@myumanitoba.ca>
2018-02-10 20:48:27 -0600

    Update with the bluetooth connection. + Put name on each MAC address + Reject the connection if it is not in the list

--- serverSide/btServerThreading.py
+++ serverSide/btServerThreading.py
@@ -15,6 +15,15 @@ def listOfCommand(command):
 		"QUIT":98
 	}.get(command, 99) # default will be WAIT
 
+def namesOfCars(MACAddress):
+    return{
+        "34:C3:D2:BF:1D:1A":"bigPine",
+        "34:C3:D2:C2:86:17":"Pen",
+        "34:C3:D2:C0:0E:6D":"Pineapple",
+        "34:C3:D2:F8:C9:50":"Apple",
+        }.get(MACAddress, "nosupport")
+
+
 class ThreadedServer(Thread):
     def __init__(self, hostMAC, port):
         #Thread.__init__(self)
@@ -32,63 +41,71 @@ class ThreadedServer(Thread):
         while True:
             client, address = self.btConnect.accept()
             client.settimeout(60)
+            #(client, (MACconnect, port)) = btConnect.accept()
             Thread(target = self.listenToClient, args = (client, address)).start()
 
     # Method to establish the new connection for each thread
     def listenToClient(self, client, clientInfo):
         dataSize = 1024
-         # First check if client finish estasblishing connection
-        client.send("ACK4S")
-        serverDataRecv = client.recv(dataSize)
-        serverDataRecv = serverDataRecv.decode("utf-8")
+         # First check if client is in our support
+        clientName = namesOfCars(str(clientInfo[0]))
 
-        #control = 0
-        control = 2
-
-        counter = 1
-        queue = []
-
-        # Second ACK the connection from client
-        if listOfCommand(serverDataRecv) == 1:
-            print("Connection established for "+str(clientInfo))
-            self.numberOfConnection += 1
-            while True:
-                # After establish connection, now start command client
-                if control == 0:
-                    queue = str(json.load(open("Traffic_Sim/Assets/CarData.json"))["commands"]).split(',')
-                    control = 1
-                    data = queue[0]
-                elif len(queue) > counter:
-                    data = queue[counter]
-                    counter = counter + 1
-                else:
-                    print("Please enter command for "+str(clientInfo)+": ")
-                    print("Available commands are UP, DOWN, LEFT, RIGHT, STOP or QUIT: ")
-                    data = input()
-
-                if listOfCommand(data) != 99:
-                # If data is valid then start sending
-
-                #Also timing the connection time
-                    start = time.time()
-                    client.send(data)
-                    
-                    # Waiting for update on position
-                    serverDataRecv = client.recv(dataSize).decode("utf-8")
-                    end = time.time()
-
-                    print("Time needed for update = " + str(end-start))
-
-                    print(serverDataRecv)
-                    #with open('Traffic_Sim/Assets/data.json', 'w') as outfile:
-                    with open('data.json', 'w') as outfile:
-                        outfile.write(serverDataRecv)
-                    # Quit when user type quit in command line
-                    if data == "QUIT":
-                        break
-                else:
-                    print("Wrong command, please check the command list and repeat input !")
+        # Reject if it's not in the system
+        if (clientName =="nosupport"):
             client.close()
+        else :
+             # Check if client finish estasblishing connection
+            client.send("ACK4S")
+            serverDataRecv = client.recv(dataSize)
+            serverDataRecv = serverDataRecv.decode("utf-8")
+        
+            #control = 0
+            control = 2
+
+            counter = 1
+            queue = []
+
+            # Second ACK the connection from client
+            if listOfCommand(serverDataRecv) == 1:
+                print("Connection established for "+clientName)
+                self.numberOfConnection += 1
+                while True:
+                    # After establish connection, now start command client
+                    if control == 0:
+                        queue = str(json.load(open("Traffic_Sim/Assets/CarData.json"))["commands"]).split(',')
+                        control = 1
+                        data = queue[0]
+                    elif len(queue) > counter:
+                        data = queue[counter]
+                        counter = counter + 1
+                    else:
+                        print("Please enter command for "+clientName+": ")
+                        print("Available commands are UP, DOWN, LEFT, RIGHT, STOP or QUIT: ")
+                        data = input()
+
+                    if listOfCommand(data) != 99:
+                    # If data is valid then start sending
+
+                    #Also timing the connection time
+                        start = time.time()
+                        client.send(data)
+                    
+                        # Waiting for update on position
+                        serverDataRecv = client.recv(dataSize).decode("utf-8")
+                        end = time.time()
+
+                        print("Time needed for update = " + str(end-start))
+
+                        print(serverDataRecv)
+                        #with open('Traffic_Sim/Assets/data.json', 'w') as outfile:
+                        with open('data.json', 'w') as outfile:
+                            outfile.write(serverDataRecv)
+                        # Quit when user type quit in command line
+                        if data == "QUIT":
+                            break
+                    else:
+                        print("Wrong command, please check the command list and repeat input !")
+                client.close()
         return False
 
 if __name__ == "__main__":
@@ -102,7 +119,7 @@ if __name__ == "__main__":
 
     #while True:
         #btConnect.listen(1)  # this will send to back log
-        #(client, (MACconnect, port)) = btConnect.accept()
+        
     newthread = ThreadedServer(hostMACAddress, btPort)
     newthread.listen()
 
